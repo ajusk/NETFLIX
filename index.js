a979001f0a2445df83b872f6d880e731b6e9043f
ajn4s <93061627+ajusk@users.noreply.github.com>
2023-06-20 11:58:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Netflix Intro Animation</title>
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
